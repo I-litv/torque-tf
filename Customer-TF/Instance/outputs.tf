c45e33f0f6c529da4265b163fe1e35ab02e35116
Ihor Litvyshchenko <dovid1989@gmail.com>
2022-11-09 13:47:44 +0200

    DNS output

--- Customer-TF/Instance/outputs.tf
+++ Customer-TF/Instance/outputs.tf
@@ -1,6 +1,6 @@
 
 output "public_dns" {
-  value = aws_instance.myapp #something not sure??
+  value = aws_instance.public_dns #something not sure??
 
   # hint: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 }
